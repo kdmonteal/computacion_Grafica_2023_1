a6f82c89ad9519b37da01cda448cfba215d1d3e0
Daniella Marin <kdmonteal@gmail.com>
2023-03-01 15:27:12 -0500

    vector draw with two potions

--- creando_escena/src/js/vectores.js
+++ creando_escena/src/js/vectores.js
@@ -1,25 +1,30 @@
-var puntoA = {
-    x: 4,
-    y: 8
-    }
+function calculateComponentVector(other,me,nameDraw) {
+    /*var puntoA = {
+        x: document.getElementById("puntoAX").value,
+        y: document.getElementById("puntoAY").value,
+        z: document.getElementById("puntoAZ").value
+        }
+    var puntoB = {
+        x: document.getElementById("puntoBX").value,
+        y: document.getElementById("puntoBY").value,
+        z: document.getElementById("puntoBZ").value
+    }*/
+    calculaVector(document.getElementById(other).value,me.value,nameDraw);
 
-var puntoB = {
-    x: 7,
-    y: 9
 }
 
-var vector = {
-    x: 0,
-    y: 0
-}
+function calculaVector(pi,pf,vr) {
 
-function calculaVector(pi,pf) {
-    vector.x = pf.x - pi.x;
-    vector.y = pf.y - pi.y;  
+    document.getElementById(vr).value = pf-pi;
     
-    console.log(vector.x+","+vector.y);
+    /*componenteX = pf.x - pi.x;
+    componenteY = pf.y - pi.y;  
+    componenteZ = pf.z - pi.z;
+
+    console.log(vector.x+","+vector.y+" , "+vector.z);*/
+
+
 }
 
-calculaVector(puntoA,puntoB);
 
 
