06e24950ff5962ab133ffa0edaba8319635a907b
Daniella Marin <kdmonteal@gmail.com>
2023-03-16 15:09:35 -0500

    spotliught

--- creando_escena/src/js/mainLights.js
+++ creando_escena/src/js/mainLights.js
@@ -147,6 +147,24 @@ function createLight(typeLight) {
         const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
         scene.add( pointLightHelper );
         break;
+    case 'SpotLight':
+        // white spotlight shining from the side, modulated by a texture, casting a shadow
+        const spotLight = new THREE.SpotLight( 0xffffff );
+        spotLight.position.set( 100, 1000, 100 );
+        spotLight.map = new THREE.TextureLoader().load( url );
+
+        spotLight.castShadow = true;
+
+        spotLight.shadow.mapSize.width = 1024;
+        spotLight.shadow.mapSize.height = 1024;
+
+        spotLight.shadow.camera.near = 500;
+        spotLight.shadow.camera.far = 4000;
+        spotLight.shadow.camera.fov = 30;
+
+        scene.add( spotLight );
+
+            break;
         
     }
 }
